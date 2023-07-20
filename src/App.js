0c0ac8593cd9c49d5a35b6e7b97978d41022ba71
Maria <nikmaria15@gmail.com>
2023-07-20 15:15:00 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Menu from "./Components/Menu";
+
+import HomePage from "./pages/HomePage";
+import DriftPage from "./pages/DriftPage";
+import ForzaPage from "./pages/ForzaPage";
+import TimeAttackPage from "./pages/TimeAttackPage";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/drift" element={<DriftPage />} />
+          <Route path="/timeattack" element={<TimeAttackPage />} />
+          <Route path="/forza" element={<ForzaPage />} />
+        </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
